af6b1dc7ad3a887ce60bbb0c87ad6ac244e2f1d3
Azlaan <azlaan472@gmail.com>
2025-03-02 01:12:04 +0530

    Add userId and imageUrl to workspace creation payload

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -40,6 +40,7 @@ const app =new Hono()
                 ID.unique(),
                 {
                     name,
+                    
                     userId:user.$id,
                     imageUrl:uploadedImageUrl,
                 },
